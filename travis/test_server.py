69059689c952cea45cf840e6c53cb02586e47949
shineit-kefei <shineit-kefei@users.noreply.github.com>
2016-08-01 17:21:48 +0800

    Update test_server.py

--- travis/test_server.py
+++ travis/test_server.py
@@ -181,8 +181,7 @@ def get_test_dependencies(addons_path, addons_list):
 
 
 def setup_server(db, odoo_unittest, tested_addons, server_path,
-                 addons_path, install_options, preinstall_modules=None,
-                 unbuffer=True):
+                 addons_path, install_options, preinstall_modules=None):
     """
     Setup the base module before running the tests
     if the database template exists then will be used.
@@ -199,15 +198,15 @@ def setup_server(db, odoo_unittest, tested_addons, server_path,
     print("\nCreating instance:")
     try:
         subprocess.check_call(["createdb","-U","odoo","-p", "5432", "-h", "postgres", db])
-        print('before psql, ssssssssss')
+        # print('before psql, ssssssssss')
         print(os.system("psql -U odoo -h postgres -p 5432 -l"))
-        print('after psql, ttttttttttt')
+        # print('after psql, ttttttttttt')
     except subprocess.CalledProcessError:
         print("Using previous openerp_template database.")
     else:
         # unbuffer keeps output colors
-        cmd_odoo = ["unbuffer"] if unbuffer else []
-        cmd_odoo += ["%s/openerp-server" % server_path,
+        # cmd_odoo = ["unbuffer"] if unbuffer else []
+        cmd_odoo = ["%s/openerp-server" % server_path,
                      "-d", db,
                      "--db_host=postgres",
                      "--db_user=odoo",
@@ -280,7 +279,7 @@ def main(argv=None):
     expected_errors = int(os.environ.get("SERVER_EXPECTED_ERRORS", "0"))
     odoo_version = os.environ.get("VERSION")
     instance_alive = str2bool(os.environ.get('INSTANCE_ALIVE'))
-    unbuffer = str2bool(os.environ.get('UNBUFFER', True))
+    # unbuffer = str2bool(os.environ.get('UNBUFFER', True))
     data_dir = os.environ.get("DATA_DIR", '~/data_dir')
     if not odoo_version:
         # For backward compatibility, take version from parameter
@@ -329,7 +328,7 @@ def main(argv=None):
         os.environ.get('TRAVIS_BUILD_DIR'))))
     print("Modules to preinstall: %s" % preinstall_modules)
     setup_server(dbtemplate, odoo_unittest, tested_addons, server_path,
-                 addons_path, install_options, preinstall_modules, unbuffer)
+                 addons_path, install_options, preinstall_modules)
                  
     
 
@@ -402,7 +401,7 @@ def main(argv=None):
                 print('in else')
                 command[-1] = to_test
                 # Run test command; unbuffer keeps output colors
-                command_call = (["unbuffer"] if unbuffer else []) + command
+                command_call = command
             print(' '.join(command_call))
             pipe = subprocess.Popen(command_call,
                                     stderr=subprocess.STDOUT,
